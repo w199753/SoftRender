74b747d2c13a19957018c52c0008797103a82699
fuziyang <fuziyang@fotoable.com>
2021-03-11 21:04:02 +0800

    no message

--- MySoftRender/SoftRender/SoftRender/Render/Camera.h
+++ MySoftRender/SoftRender/SoftRender/Render/Camera.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include<glm/vector_relational.hpp>
+#include<glm/matrix.hpp>
 namespace softRD
 {
 	class Camera
@@ -8,7 +9,21 @@ namespace softRD
 		Camera() {}
 		~Camera() {}
 
-	private:
+		void SetCameraParams()
+		{
 
-	}
+		}
+	private:
+		glm::vec3 worldUpDir = glm::vec3(0, 1, 0);
+
+		//透视投影所需参数
+		float near, far, fov,aspect;    //这里fov为纵向，aspect为宽高比
+		//相机viewportRect参数
+		float X, Y, W, H;
+
+		glm::vec3 up, right, forward;
+		glm::mat4x4 viewMatrix;
+		glm::mat4x4 projectMatrix;
+		glm::mat4x4 viewportMatrix;
+	};
 }
